b35d4f51ca952d3f79ac7d8c7b747da123d693f4
Ivan Zheng <zhengivan10@gmail.com>
2023-04-14 08:54:03 +0200

    Se crea y empieza Ejercicio4practicarListasdeListas

---
+++ ejercicio4PracticarListasDeListas/ejercicio4PracticarListasDeListas/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ejercicio4PracticarListasDeListas
+//
+//  Created by Ivan Zheng on 14/4/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
